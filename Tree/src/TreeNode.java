fb04093557d38681f9ba3ce153a772f4397c9c9a
Aviv Reuven <aviv1204@gmail.com>
2021-06-17 15:37:29 +0300

    create getter and setter for TreeNode create print functions for Tree

---
+++ Tree/src/TreeNode.java
@@ -0,0 +1,57 @@
+public class TreeNode {
+    private TreeNode left;
+    private TreeNode right;
+    private int data;
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public void inOrderPrint() {
+        if (left != null){
+            left.inOrderPrint();
+        }
+        System.out.print(data);
+        if (right != null){
+            right.inOrderPrint();
+        }
+    }
+    public void preOrderPrint() {
+        System.out.print(data);
+        if (left != null){
+            left.inOrderPrint();
+        }
+        if (right != null){
+            right.inOrderPrint();
+        }
+    }
+    public void postOrderPrint() {
+        if (left != null){
+            left.inOrderPrint();
+        }
+        if (right != null){
+            right.inOrderPrint();
+        }
+        System.out.print(data);
+    }
+}
