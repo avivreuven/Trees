b11de75562965957553353551e7bf042a6e07b06
Aviv Reuven <aviv1204@gmail.com>
2021-06-17 23:49:21 +0300

    update

--- Tree/src/TreeMirror.java
+++ Tree/src/TreeMirror.java
@@ -1,5 +1,6 @@
 public class TreeMirror {
-    public TreeNode treeMirror (TreeNode root){
+
+    public static TreeNode treeMirror (TreeNode root){
         if (root == null){
             return null;
         }
@@ -7,5 +8,18 @@ public class TreeMirror {
         node.setLeft(treeMirror(root.getRight()));
         node.setRight(treeMirror(root.getLeft()));
         return node;
+    }
+
+    public static void main(String []arg){
+        TreeNode node4 = new TreeNode(null,null,4);
+        TreeNode node5 = new TreeNode(null,null,5);
+        TreeNode node6 = new TreeNode(null,null,6);
+        TreeNode node7 = new TreeNode(null,null,7);
+        TreeNode node2 = new TreeNode(node4,node5,2);
+        TreeNode node3 = new TreeNode(node6,node7,3);
+        TreeNode node1 = new TreeNode(node2,node3,1);
+        node1.inOrderPrint();
+        TreeNode mirror = treeMirror(node1);
+        mirror.inOrderPrint();
     }
 }
